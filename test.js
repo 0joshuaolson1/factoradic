e07dc52b5f074fc37c6bd2501869ccc9ff1124e9
Joshua Olson <0joshuaolson1@gmail.com>
2017-12-13 14:47:51 -0700

    test.js should omit, not use 'null', baka

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 require('./index.js').test(
   8,
-  function(){console.log('factoradic tests passed')},
-  null // default
+  function(){console.log('factoradic tests passed')}
+  // last argument's (onfail) default logs some failure info to console.error
 )
