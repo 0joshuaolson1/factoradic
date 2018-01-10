4e5600e92e98da421f45987d1c6fff30005708f2
Joshua Olson <0joshuaolson1@gmail.com>
2018-01-09 20:29:57 -0700

    Le comments

--- index.js
+++ index.js
@@ -5,33 +5,37 @@ function loop(i, end, body){
   for(var step = i<end ? 1 : -1; i-end != step; i += step) body(i);
 }
 
+exports.loop = loop; // just for you; no need to thank
+
+function muladdImpl(n, m, a){return n*m + a;};
+function divmodImpl(n, d){return {div:Math.floor(n/d), mod:n%d}};
+
 exports.ptoa = function(p){
   var indexof = Array(p.length);
   p.forEach(function(val, i){indexof[val] = i;});
   var a = Array(p.length-1);
   loop(a.length, 1, function(i){
-    p[indexof[i]] = p[i];
-    indexof[p[i]] = indexof[i];
+    p[indexof[i]] = p[i]; // unnecessary in the last 2 loops
+    indexof[p[i]] = indexof[i]; // unnecessary in the last loop
     a[i-1] = i-indexof[i];
   });
   return a;
 }
-exports.aton = function(a, zero, muladd){
+
+// ptoa and aton combined with a fused loop and no intermediate a
+exports.pton = function(p, zero, muladd){
   var n = zero || 0;
-  muladd = muladd || function(n, m, a){return n*m + a;};
-  loop(a.length+1, 2, function(radix){n = muladd(n, radix, a[radix-2]);});
+  muladd = muladd || muladdImpl;
+  var indexof = Array(p.length);
+  p.forEach(function(val, i){indexof[val] = i;});
+  loop(p.length-1, 1, function(i){
+    p[indexof[i]] = p[i];
+    indexof[p[i]] = indexof[i];
+    n = muladd(n, i+1, i-indexof[i]);
+  });
   return n;
 }
-exports.ntoa = function(n, maxRadix, divmod){
-  divmod = divmod || function(n, d){return {div:Math.floor(n/d), mod:n%d}};
-  var a = Array(maxRadix-1);
-  loop(2, maxRadix, function(radix){
-    var dm = divmod(n, radix);
-    a[radix-2] = dm.mod;
-    n = dm.div;
-  });
-  return a;
-}
+
 exports.atop = function(a, p){
   p = p || Array.from(Array(a.length+1).keys());
   loop(1, a.length, function(i){
@@ -42,20 +46,19 @@ exports.atop = function(a, p){
   return p;
 }
 
-exports.pton = function(p, zero, muladd){
+exports.aton = function(a, zero, muladd){
   var n = zero || 0;
-  muladd = muladd || function(n, m, a){return n*m + a;};
-  var indexof = Array(p.length);
-  p.forEach(function(val, i){indexof[val] = i;});
-  loop(p.length-1, 1, function(i){
-    p[indexof[i]] = p[i];
-    indexof[p[i]] = indexof[i];
-    n = muladd(n, i+1, i-indexof[i]);
-  });
+  muladd = muladd || muladdImpl;
+
+  // muladd is overkill in the first loop, but the zero method is better in pton
+  loop(a.length+1, 2, function(radix){n = muladd(n, radix, a[radix-2]);});
+
   return n;
 }
+
+// ntoa and atop combined with a fused loop and no intermediate a
 exports.ntop = function(n, maxRadix, p, divmod){
-  divmod = divmod || function(n, d){return {div:Math.floor(n/d), mod:n%d}};
+  divmod = divmod || divmodImpl;
   p = p || Array.from(Array(maxRadix).keys());
   loop(1, p.length-1, function(i){
     var dm = divmod(n, i+1);
@@ -66,6 +69,17 @@ exports.ntop = function(n, maxRadix, p, divmod){
   });
   return p;
 };
+
+exports.ntoa = function(n, maxRadix, divmod){
+  divmod = divmod || divmodImpl;
+  var a = Array(maxRadix-1);
+  loop(2, maxRadix, function(radix){
+    var dm = divmod(n, radix); // divmod is overkill in the last loop, since
+    a[radix-2] = dm.mod; //       mod == n if the original n < maxRadix!
+    n = dm.div; //             <- and this is unnecessary in the last loop
+  });
+  return a;
+}
 
 exports.test = function(maxMaxRadix, onpass, onfail){
   var failed = false, factorial = 1;
