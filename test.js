f9a6333a7d6ea71d473455d2a9568e631737448f
Joshua Olson <0joshuaolson1@gmail.com>
2018-01-10 18:10:06 -0700

    Simplify

--- test.js
+++ test.js
@@ -1,5 +1,1 @@
-require('./index.js').test(
-  8,
-  function(){console.log('factoradic tests passed')}
-  // last argument's (onfail) default logs some failure info to console.error
-)
+require('./index.js').test()
